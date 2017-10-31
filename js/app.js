06d772d4e6499ea8fe4c50e9858552cf17e27190
miloaxelrod <miloaxelrod@gmail.com>
2017-10-30 21:59:13 -0400

    WE MADE IT WORK ON THE FIRST TRY

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
-/*
-  Please add all Javascript code to this file.
-*/
+var root = 'https://accesscontrolalloworiginall.herokuapp.com/http://mashable.com/stories.json';
+
+$.ajax({
+  url: root,
+  method: 'GET'
+}).then(function(data) {
+    for (var i = 0; i < data.hot.length; i++){
+
+      var html = '<article class="article"><section class="featuredImage"><img src="' + data.hot[i].image + '" alt="" /></section><section class="articleContent"><a href="#"><h3>' + data.hot[i].title + '</h3></a><h6>' + data.hot[i].channel + '</h6></section><section class="impressions">' + data.hot[i].shares.total + '</section><div class="clearfix"></div></article>'
+
+      $('#main').append(html);
+
+    }
+});
